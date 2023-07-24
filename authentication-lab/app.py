e37cc321ef9ca3ee35a5ad0e96b24276ecae034e
Nadeengh24-meet <139760345+Nadeengh24-meet@users.noreply.github.com>
2023-07-24 14:13:19 +0300

    still working

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -13,13 +13,51 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try: 
+            login_session['user'] = auth.create_user_with_email_and_password(email,password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = 'Authentication failed'
     return render_template("signup.html")
-
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
 
+
+@app.route("/signin", methods= ['GET', 'POST'])
+def signin():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try: 
+            login_session['user'] = auth.sign_in_with_email_and_password(email,password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = 'Authentication failed'
+    return render_template("signin.html")
+
+
+    
+Config = {
+
+  "apiKey": "AIzaSyChVjCGltcEOF3rDZe12kEnHqBEdBhO388",
+  "authDomain": "nadeen-79c65.firebaseapp.com",
+  "projectId": "nadeen-79c65",
+  "storageBucket": "nadeen-79c65.appspot.com",
+  "messagingSenderId": "15669696773",
+  "appId": "1:15669696773:web:b1e34f1fccd4d9b865bf6c",
+  "measurementId": "G-L9PB2L26XY", 
+  "databaseURL": ""
+
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 if __name__ == '__main__':
     app.run(debug=True)
